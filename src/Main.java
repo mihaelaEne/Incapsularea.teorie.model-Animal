b0f56c902dcddf46d2c11c27fd4a9426633e87d2
ENE MIHAELA <mihaela.een@gmail.com>
2023-03-19 16:58:12 +0200

    +Oras+Animal

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
 import model.Animal;
+import model.Oras;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal();
+//        Animal animal = new Animal();//un constructor se apealza la instantiere
+//        //adica cand cream un obiect
+//        //adica folosim cuvantul cheie new
+//
+//        Animal ex= new Animal("adsadas");
+//
+//        animal.setNume("Sasha");
+//        animal.setId(001);
+//        animal.setVarsta(10);
+//        animal.setCuloare("Maro");
+//        animal.setLocatieAnterioara("Brasov");
+//        animal.setLocatieActuala("sinaia");
+//
+//        System.out.println(animal.getNume());
+//        System.out.println(animal.getId());
+//        System.out.println(animal.getVarsta());
+//        System.out.println(animal.getCuloare());
+//        System.out.println(animal.getLocatieAnterioara());
+//        System.out.println(animal.getLocatieActuala());
+//
+//
+//       Animal a3= new Animal("test",12,2,"culoare","test2");
+//
 
-        animal.setNume("test");
+//        Animal a4 = new Animal("Ares", 007, 1, "blue", "Medias", "Bucuresti");
+//        System.out.println(a4.descriereAnimal());
+//
+//        System.out.println("\n");
+//        a4.setNume("Ares A");
+//        System.out.println(a4.descriereAnimal());
+//
+//
 
+        Oras o1=new Oras("Bucuresti",3000000, 85);
+        Oras o2=new Oras("Alexandria", "Teleorman",500000, 45);
 
-
-
-
-        System.out.println(animal.getNume());
-
-
-
+        System.out.println(o1.descriereOras());
+        System.out.println("\n");
+        System.out.println(o2.descriereOras());
 
     }
 }
---
+++ src/controllers/AnimalController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import model.Animal;
+
+import java.util.ArrayList;
+
+public class AnimalController {
+
+    private ArrayList<Animal> animals;
+
+    public AnimalController(){
+
+        this.animals= new ArrayList<>();
+    }
+
+    public void load(){
+        Animal a1=new Animal("Pablo", 123456,6,"alb","Pantelimon", "Dristor");
+        Animal a2=new Animal("Burito", 789456, 5,"negru","Corbeanca","Ilfov");
+        Animal a3=new Animal("Mario", 741258963, 8,"galben", "Pitesti", "Ploiesti");
+
+        animals.add(a1);
+        animals.add(a2);
+        animals.add(a3);
+
+    }
+
+    public void afisareAnimal(){
+        for(int i=0; i<animals.size(); i++){
+            System.out.println(this.animals.get(i).descriereAnimal());
+            System.out.println("\n");
+        }
+    }
+}
